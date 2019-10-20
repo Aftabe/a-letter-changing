61f72f6931e0db9097107d094a05d8e487bc6d61
Ali Khan <shamsasghari77@gmail.com>
2019-10-20 15:45:31 +1100

    new function added, converting text to uppercase letter

--- js/style.js
+++ js/style.js
@@ -2,4 +2,10 @@ function doSomething() {
     var str = document.getElementById('demo').innerHTML;
     var text = str.replace('language','toturial');
     document.getElementById('demo').innerHTML = text;
+};
+
+function myFunction() {
+    var str1 = document.getElementById('demo1').innerHTML;
+    var text1 = document.getElementById('demo1').innerHTML = str1.toUpperCase();
+    
 }
